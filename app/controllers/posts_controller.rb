1f601edf91d107952c74fe2b77b12cac5d4cc07a
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-18 15:50:31 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,8 +24,10 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)
+
+		@post.update(params.require(:post).permit(:title, :description))
+	  #@post = Post.find(params[:id])
+	  #@post.update(title: params[:title], description: params[:description])
+	  #redirect_to post_path(@post)
 	end
 end
